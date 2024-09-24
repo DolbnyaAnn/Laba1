7ee7dfc69837bec8284a1b4466d3288685bad775
Anna <dolbna.anna453@gmail.com>
2024-09-24 15:11:44 +0700

    CaP

--- src/main/java/z2.java
+++ src/main/java/z2.java
@@ -1,14 +1,14 @@
 public class z2 {
     public static void main(String[] args) {
 
-        System.out.println(z__2("Boing-777"));
+        System.out.println(z__2("Boeing-777"));
     }
 
-    public static String z__2(String b) {
-        String m="";
-        for (int i = 0; i < b.length(); i++) {
-                m+=b.charAt(b.length() - i-1);
+    public static String z__2(String str) {
+        String new_str="";
+        for (int i = 0; i < str.length(); i++) {
+                new_str+=str.charAt(str.length() - i-1);
         }
-        return m;
+        return new_str;
     }
 }
