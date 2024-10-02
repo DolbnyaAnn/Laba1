f6b148b4abbfe6770da353bcb391c1bca55db5fc
Anna <dolbna.anna453@gmail.com>
2024-10-02 12:09:38 +0700

    CaP

--- src/main/java/laba2/Button.java
+++ src/main/java/laba2/Button.java
@@ -1,5 +1,12 @@
 package laba2;
 
 public class Button {
-
+    int count;
+    public Button() {
+        this.count = 0;
+    }
+    public void click(){
+        count++;
+        System.out.println("Button was clicked "+count+" times");
+    }
 }
